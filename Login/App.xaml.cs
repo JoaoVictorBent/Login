a3f70a462101ae120baeacd92d84d130d849a5c8
jhonnezada420 <jhonnezada420@JHONNEZADA>
2025-08-20 01:24:46 -0300

    Adicionar arquivos de projeto.

---
+++ Login/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Login
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+        }
+
+
+        //Método para mudar resolução da janela
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 700;
+
+            return window;
+
+        }
+
+    }
+}
